e412a917db10817da845b4653d5faaf5cf41c66c
masTech Developer <ashir342@gmail.com>
2020-05-19 17:20:50 +0500

    setup enviroment

---
+++ app.js
@@ -0,0 +1,371 @@
+// Contract Address : 0xF9309F224C89785C66d75f3cE2AA28cD446Fc827
+
+var Contract;
+
+var address = '0xF9309F224C89785C66d75f3cE2AA28cD446Fc827';
+
+var abi = [
+  {
+    inputs: [
+      {
+        internalType: 'string',
+        name: '_tokenName',
+        type: 'string',
+      },
+      {
+        internalType: 'string',
+        name: '_symbol',
+        type: 'string',
+      },
+      {
+        internalType: 'uint256',
+        name: '_totalSupply',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: '_decimals',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'tokenOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'tokens',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokens',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: 'success',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'send_token',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'token',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: 'success',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'tokens',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokens',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferFrom',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: 'success',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_price',
+        type: 'uint256',
+      },
+    ],
+    name: 'update_price',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_owner',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+    ],
+    name: 'allowance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'tokenOwner',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+$(document).ready(function () {
+  web3 = new Web3(web3.currentProvider);
+
+  Contract = new web3.eth.Contract(abi, address);
+
+  Contract.methods
+    .totalSupply()
+    .call()
+    .then(function (sup) {
+      $('#tSupply').html(sup);
+    });
+  Contract.methods
+    .name()
+    .call()
+    .then(function (name) {
+      $('#name').html(name);
+    });
+
+  Contract.methods
+    .name()
+    .call()
+    .then(function (name) {
+      $('#name').html(name);
+    });
+});
+s;
+
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
+
+  start: async function () {
+    const { web3 } = this;
+
+    try {
+      this.meta = new web3.eth.Contract(abi, address);
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+      this.getBalance();
+    } catch (error) {
+      console.log('Could not connect to contract or chain.');
+    }
+  },
+
+  getBalance: async function () {
+    // Get Metamask Account
+    web3.eth.getAccounts().then(function (acco) {
+      $('#accountAddress').html(acco);
+    });
+
+    // Call getBalance function
+    this.meta.methods
+      .getBalance()
+      .call()
+      .then(function (bal) {
+        $('#bala').html(bal);
+      });
+  },
+};
+
+window.App = App;
+
+window.addEventListener('load', function () {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn(
+      'No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live'
+    );
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider('http://127.0.0.1:8545')
+    );
+  }
+
+  App.start();
+});
